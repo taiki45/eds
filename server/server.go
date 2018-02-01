5783699827d251382a5b2b0e6b0f673e76b8d4ea
Taiki Ono <taiks.4559@gmail.com>
2018-02-01 18:18:39 +0900

    add-endpoint

--- server/server.go
+++ server/server.go
@@ -5,10 +5,12 @@ import (
 	"fmt"
 	"log"
 	"net"
+	"time"
 
 	"github.com/envoyproxy/go-control-plane/api"
 	"github.com/envoyproxy/go-control-plane/pkg/cache"
 	xds "github.com/envoyproxy/go-control-plane/pkg/server"
+	"github.com/gogo/protobuf/proto"
 	"github.com/golang/glog"
 	"google.golang.org/grpc"
 )
@@ -21,8 +23,8 @@ type nodeGroupHasher struct {
 }
 
 // Hash function that always returns same value.
-func (h nodeGroupHasher) Hash(node *api.Node) (cache.Key, error) {
-	return cache.Key(node.Id), nil
+func (h nodeGroupHasher) Hash(*api.Node) (cache.Key, error) {
+	return cache.Key("TODO"), nil
 }
 
 // Run server
@@ -36,6 +38,50 @@ func Run() {
 }
 
 func runResouceUpdator(ctx context.Context, c cache.Cache) {
+	i := 0
+	for {
+		version := fmt.Sprintf("version%d", i)
+		endpoint := makeEndpoint()
+
+		glog.Infof("updating cache with %d-labelled responses", i)
+		snapshot := cache.NewSnapshot(version,
+			[]proto.Message{endpoint},
+			[]proto.Message{},
+			[]proto.Message{},
+			[]proto.Message{})
+		c.SetSnapshot(cache.Key("TODO"), snapshot)
+
+		select {
+		case <-time.After(10 * time.Second):
+		case <-ctx.Done():
+			return
+		}
+		i++
+	}
+
+}
+
+func makeEndpoint() *api.ClusterLoadAssignment {
+	return &api.ClusterLoadAssignment{
+		ClusterName: "test",
+		Endpoints: []*api.LocalityLbEndpoints{{
+			LbEndpoints: []*api.LbEndpoint{{
+				Endpoint: &api.Endpoint{
+					Address: &api.Address{
+						Address: &api.Address_SocketAddress{
+							SocketAddress: &api.SocketAddress{
+								Protocol: api.SocketAddress_TCP,
+								Address:  "127.0.0.1",
+								PortSpecifier: &api.SocketAddress_PortValue{
+									PortValue: 3000,
+								},
+							},
+						},
+					},
+				},
+			}},
+		}},
+	}
 }
 
 // RunGrpcServer starts an xDS server at the given port.
